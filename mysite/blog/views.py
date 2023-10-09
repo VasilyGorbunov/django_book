3c767ca39f8d1b8e145575b2946f5a755c0e4a74
VasilyGorbunov <vasily.gorbunov@gmail.com>
2023-10-09 14:24:21 +0300

    page 87

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Post
+
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request, 'blog/post/list.html', {'posts': posts})
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(
+        Post,
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day
+    )
+
+    return render(request, 'blog/post/detail.html', {'post': post})
